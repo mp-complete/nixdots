b68478e38b701204bbb6b33bc908b5fc73f239d9
Miles Possing <mp-complete@pm.me>
2026-08-01 14:08:22 -0500

    update pi discovery

--- modules/ai/extensions/pi-copilot-discovery.nix
+++ modules/ai/extensions/pi-copilot-discovery.nix
@@ -6,7 +6,7 @@
   # https://github.com/milespossing/pi-copilot-discovery
   pi.extensions.pi-copilot-discovery = {
     pname = "@milespossing/pi-copilot-discovery";
-    version = "0.3.2";
-    hash = "sha512-pJHUaMmAbHiylPDZEeeK+U//hl6f4ybSxiAg34PwT7cyxL9+Ik7phDGeYs4FBoVjLFlAZ/7rU5cVZmZ0pZ/3rQ==";
+    version = "0.4.1";
+    hash = "sha512-KBigNb8IP3rhxdFr+8V9TLsNTZQ3vYHhPkJ+VLEpB+zCmYRAn65PGpF7E6racNDkWlhaLJbjx+FvSm378P1aNA==";
   };
 }
