01a28f94d1cae5f442afe858bc65317f5d3e9bc3
Miles Possing <mp-complete@pm.me>
2026-08-13 10:43:40 -0500

    wt repo update

--- modules/development/repositories/trident-warehouse.nix
+++ modules/development/repositories/trident-warehouse.nix
@@ -8,4 +8,9 @@
       "playwright"
     ];
   };
+
+  flake.wrappers.worktrunk.settings.projects."dev.azure.com/powerbi/Trident/_git/TridentWarehouse-UX" = {
+    worktree-path = "{{ repo_path }}/../{{ branch | sanitize }}";
+    post-start = "yarn install && yarn build";
+  };
 }
